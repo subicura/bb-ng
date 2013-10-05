9c81cc1bf1ef4772cb099dea060ef96675598dca
chungsub.kim <chungsub.kim@purpleworks.co.kr>
2013-10-06 08:52:39 +0900

    init project

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('bbNgApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
