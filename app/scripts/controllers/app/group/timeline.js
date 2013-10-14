f43e3d422885bb82ecc8a0d1dcdf8c763c5baedd
subicura <subicura@subicura.com>
2013-10-15 01:58:35 +0900

    오늘은 여기까지 -ㅁ-/

--- app/scripts/controllers/app/group/timeline.js
+++ app/scripts/controllers/app/group/timeline.js
@@ -4,10 +4,12 @@ angular.module('bbNgApp')
   .controller('AppGroupTimelineCtrl', function ($scope, $http, $state, groupService) {
     $scope.editGroupForm = {};
     $scope.form = {};
+    $scope.community = groupService.get({ id:$state.params.community_id });
     $scope.stats = $http.get('http://localhost:3000/groups/'+ $state.params.community_id +'/bookkeepings/2013-10-01/2013-10-31/calculate').then(function(result) {return result.data});
     $scope.bookkeepings = $http.get('http://localhost:3000/groups/'+ $state.params.community_id +'/bookkeepings').then(function(result) { return result.data;});
     $scope.account_titles = $http.get('http://localhost:3000/account_titles').then(function(result) { return result.data; });
     $scope.group_members = $http.get('http://localhost:3000/groups/' + $state.params.community_id + '/members').then(function(result) { return result.data; });
+
     $scope.removeBookkeeping = function(index) {
       if(confirm("Are you sure?"))
       {
@@ -28,30 +30,25 @@ angular.module('bbNgApp')
           $scope.stats = $http.get('http://localhost:3000/groups/'+ $state.params.community_id +'/bookkeepings/2013-10-01/2013-10-31/calculate').then(function(result) {return result.data});
       });
     };
-    if($state.params.community_id) {
-      groupService.get({ id:$state.params.community_id }, function(data) {
-        $scope.community = data;
-      });
 
-      $scope.showEditGroup = function(group) {
-        $scope.editGroupForm = angular.copy($scope.community);
-        $('.edit.group.modal')
-          .modal('setting', 'selector', {
-            close : '.close, .actions .cancel.button'
-          })
-          .modal('setting', 'transition', 'fade up')
-          .modal('show');
-      }
+    $scope.showEditGroup = function(group) {
+      $scope.editGroupForm = angular.copy($scope.community);
+      $('.edit.group.modal')
+        .modal('setting', 'selector', {
+          close : '.close, .actions .cancel.button'
+        })
+        .modal('setting', 'transition', 'fade up')
+        .modal('show');
+    }
 
-      $scope.editGroup = function() {
-        if($.trim($scope.community.name) == "") {
-          alert("그룹명을 입력해주세요.");
-        } else {
-          $scope.editGroupForm.$update(function(data) {
-            $scope.community = data;
-            $('.edit.group.modal').modal('hideDimmer');
-          });
-        }
+    $scope.editGroup = function() {
+      if($.trim($scope.community.name) == "") {
+        alert("그룹명을 입력해주세요.");
+      } else {
+        $scope.editGroupForm.$update(function(data) {
+          $scope.community = data;
+          $('.edit.group.modal').modal('hideDimmer');
+        });
       }
     }
   });
