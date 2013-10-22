8bff8ddd8e2f5edf85b060e4477ca0527f493076
subicura <subicura@subicura.com>
2013-10-22 19:08:14 +0900

    add some test code

--- test/spec/controllers/app/setting.js
+++ test/spec/controllers/app/setting.js
@@ -6,36 +6,47 @@ describe('Controller: AppSettingCtrl', function () {
   beforeEach(module('bbNgApp'));
 
   var AppSettingCtrl,
-    scope;
+    scope,
+    LoginInfo;
+
+  // set LoginInfo
+  beforeEach(inject(function ($injector) {
+    LoginInfo = $injector.get("LoginInfo");
+    LoginInfo.setUserInfo({
+      id:1,
+      username:"subicura",
+      email:"subicura@subicura.com",
+      avatar:"/public/default.png",
+      auth_token:"abcdefg"
+    });
+  }));
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, LoginInfo) {
+  beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     AppSettingCtrl = $controller('AppSettingCtrl', {
       $scope: scope
     });
-
-    LoginInfo.setUserInfo({
-      id:1,
-      username:"subicura",
-      email:"email",
-      avatar:"/public/default.png",
-      auth_token:"abcdefg"
-    });
-    
   }));
+
+  // cleanup LoginInfo
+  afterEach(function () {
+    LoginInfo.reset();
+  });
 
   it('should have a AppSettingCtrl controller', function() {
     expect(AppSettingCtrl).toBeDefined();
   });
 
-  it('should have $scope.user', function() {
+  it('should be defined $scope.user', function() {
     expect(scope.user).toBeDefined();
   });
 
-  it('$scope.user should equal to LoginInfo.currentUser', inject(['LoginInfo',
-    function(LoginInfo) {
-      expect(scope.user).toEqual(LoginInfo.currentUser);
-    }])
-  );
+  it('should be defined $scope.userFormSubmit', function() {
+    expect(scope.userFormSubmit).toBeDefined();
+  });
+
+  it('$scope.user should equal to LoginInfo.currentUser', function() {
+    expect(scope.user).toEqual(LoginInfo.currentUser);
+  });
 });
