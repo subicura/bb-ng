ba855c4d8ed73673a0f12938bb6401844286d043
subicura <subicura@subicura.com>
2013-10-22 22:47:45 +0900

    add test port

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,6 +93,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
+          port: 9001,
           middleware: function (connect) {
             return [
               mountFolder(connect, '.tmp'),
