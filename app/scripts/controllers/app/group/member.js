e01ec95f8f1b901fd5f18169ad93edf0d78c1893
Jaehue Jang <bbugguj@Jaehue-ui-MacBook-Pro.local>
2013-10-30 09:55:28 +0900

    그룹 멤버 조회(진행중)

--- app/scripts/controllers/app/group/member.js
+++ app/scripts/controllers/app/group/member.js
@@ -1,5 +1,24 @@
 'use strict';
 
 angular.module('bbNgApp')
-  .controller('AppGroupMemberCtrl', function ($scope) {
+  .controller('AppGroupMemberCtrl', function ($scope, $state, GroupService, UserService) {
+
+  	$scope.search = "";
+  	$scope.searchText = "";
+  	$scope.members = GroupService.members({ id: $state.params.group_id });  	
+  	$scope.users = UserService.query();
+
+  	$scope.searchUsers = function(){
+  		UserService.search({search: $scope.searchText}, function(data){
+  			$scope.users = data;
+  		});
+  	}
+
+    $scope.joinUsers = function(){
+      for (var i = $scope.users.length - 1; i >= 0; i--) {
+        if($scope.users[i].checked)
+          GroupService.add({id: $state.params.group_id, collection_id:$scope.users[i].id});
+      };
+      
+    }
   });
