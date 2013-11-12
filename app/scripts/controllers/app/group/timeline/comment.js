4da0d1f0e20d00f85aae905f788ab0e72f344f7b
han <hgk617@naver.com>
2013-11-11 22:21:07 -0800

    더보기 기능구현 완료

--- app/scripts/controllers/app/group/timeline/comment.js
+++ app/scripts/controllers/app/group/timeline/comment.js
@@ -3,6 +3,26 @@
 angular.module('bbNgApp')
   .controller('AppGroupTimelineCommentCtrl', function ($scope, $state, CommentService) {
 
+    $scope.bookkeeping_comments = CommentService.get({
+      group_id: $state.params.group_id,
+      commentable_type: 'bookkeepings',
+      commentable_id: $scope.bookkeeping.id,
+    });
+
+    $scope.more = function(next){
+      CommentService.get({
+        group_id: $state.params.group_id,
+        commentable_type: 'bookkeepings',
+        commentable_id: $scope.bookkeeping.id,
+        page: next,
+      }, function(data){
+        angular.forEach(data.comments, function(value, key){
+          $scope.bookkeeping_comments.comments.push(value);
+        });
+        $scope.bookkeeping_comments.next = data.next;
+      });
+    }
+
     function initForm() {
       $scope.newComment = {};
       $scope.commentForm.$setPristine();
@@ -17,7 +37,7 @@ angular.module('bbNgApp')
           commentable_id: $scope.bookkeeping.id,
           comment: $scope.newComment
         }, function(data) {
-          $scope.bookkeeping.comments.push(data);
+          $scope.bookkeeping_comments.comments.push(data);
           initForm();
         });
       } else {
@@ -26,7 +46,7 @@ angular.module('bbNgApp')
     };
 
     $scope.removeComment = function(idx) {
-      var comment_id = $scope.bookkeeping.comments[idx].id;
+      var comment_id = $scope.bookkeeping_comments.comments[idx].id;
       if(confirm("Are you sure?")) {
         CommentService.remove({
           group_id: $state.params.group_id,
@@ -34,7 +54,7 @@ angular.module('bbNgApp')
           commentable_id: $scope.bookkeeping.id,
           comment_id: comment_id
         }, function(data) {
-          $scope.bookkeeping.comments.splice(idx, 1);
+          $scope.bookkeeping_comments.comments.splice(idx, 1);
         });
       }
     };
