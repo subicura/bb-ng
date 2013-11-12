e8e721bdc219a13a7ed74b94b9721e87e31e64df
Hyo Seong Choi <rorlab@gmail.com>
2013-11-12 17:32:20 +0900

    거래 내역 보기

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bbNgApp', ['config', 'ngResource', 'resource.plus', 'ngAnimate', 'ui.router', 'ui.date', 'facebook', 'ngx', 'ui.calendar'])
+angular.module('bbNgApp', ['config', 'ngResource', 'resource.plus', 'ngAnimate', 'ui.router', 'ui.date', 'facebook', 'ngx', 'ui.calendar', 'ngGrid'])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, FacebookProvider, CONFIG) {
     // default route
     $urlRouterProvider.otherwise("/");
--- app/scripts/controllers/app/group/item.js
+++ app/scripts/controllers/app/group/item.js
@@ -1,29 +1,59 @@
 'use strict';
 
 angular.module('bbNgApp')
-  .controller('AppGroupItemCtrl', function ($scope, $state, GroupService, BookkeepingService, AccountTitleService) {
-    $scope.form = {};
+  .controller('AppGroupItemCtrl', function ($scope, $state, GroupService, BookkeepingService) {
     $scope.stats = BookkeepingService.calculate({ 
       group_id:$state.params.group_id,
       start_date:moment().startOf('month').format("YYYY-MM-DD"),
       end_date:moment().endOf('month').format("YYYY-MM-DD")
     });
-    $scope.bookkeepings = BookkeepingService.query({ group_id:$state.params.group_id });
-    $scope.account_titles = AccountTitleService.query();
+    $scope.bookkeeping_list = [];
+    $scope.bookkeepings = BookkeepingService.query({ group_id:$state.params.group_id }, function(data){
+      console.log(data);
+      for (var i=0;i<data.length;i++)
+      {
+        console.log(data);
+        $scope.bookkeeping_list[i] = {
+          issue_date: data[i].issue_date,
+          remark: data[i].remark,
+          account_title: data[i].account_title.title,
+          amount: numberWithCommas(((data[i].operator == "-") ? data[i].amount * -1 : data[i].amount)),
+          issuer: data[i].issuer.username
+        };
+      };
+
+    });
     $scope.group_members = GroupService.members({ id:$state.params.group_id });
 
     $scope.termSubmit = function() {
       var start_date = moment($scope.term.start_date).format("YYYY-MM-DD");
       var end_date = moment($scope.term.end_date).format("YYYY-MM-DD");
-      $scope.stats = BookkeepingService.calculate({ 
+      $scope.stats = BookkeepingService.calculate({
         group_id:$state.params.group_id,
         start_date:start_date,
         end_date:end_date
       });
-      $scope.bookkeepings = BookkeepingService.query({ 
+      $scope.bookkeepings = BookkeepingService.query({
         group_id:$state.params.group_id,
         start_date:start_date,
         end_date:end_date
       });
     };
+
+    // https://github.com/angular-ui/ng-grid/wiki/Defining-columns
+    $scope.gridOptions = {
+      data: 'bookkeeping_list',
+      columnDefs: [
+        {field: 'issue_date', displayName: '거래일', width: 120, headerClass:'center' },
+        {field: 'remark',     displayName: '적요', headerClass:'center' },
+        {field: 'account_title',    displayName: '계정항목', width:120, headerClass:'center' },
+        {field: 'amount',     displayName: '금액', width:200, cellClass:'right', headerClass:'center', cellTemplate: '<div ng-class="{red: row.getProperty(col.field) < 0}"><div class="ngCellText">₩ {{row.getProperty(col.field)}}</div></div>' },
+        {field: 'issuer',     displayName: '거래자', width:150, cellClass:'center', headerClass:'center' }
+      ]
+    };
   });
+
+// http://stackoverflow.com/a/2901298
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
