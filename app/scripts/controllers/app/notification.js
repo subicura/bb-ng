4851e298b0ef3f49e0efd4045868e5e47c7aefe8
Kim Daekwon <propellerheaven@gmail.com>
2013-11-17 23:40:44 +0900

    Public_activity 기본 뷰 추가

--- app/scripts/controllers/app/notification.js
+++ app/scripts/controllers/app/notification.js
@@ -1,5 +1,20 @@
 'use strict';
 
 angular.module('bbNgApp')
-  .controller('AppNotificationCtrl', function ($scope) {
+  .controller('AppNotificationCtrl', function ($scope, ActivityService) {
+    $scope.activity = {};
+    $scope.activities = ActivityService.query({});
+    $scope.activity_message = function (activity){
+      if (activity.trackable_type == "Comment"){
+        return "새로운 댓글을 달았습니다.";
+      } else if(activity.trackable_type == "Bookkeeping") {
+        return "새로운 가계부 항목을 입력했습니다.";
+      }
+      return "";
+    };
+
+    $scope.templates =
+      [ { name: 'template1.html', url: 'comment.html'}
+        , { name: 'template2.html', url: 'bookkeeping.html'} ];
+    $scope.template = $scope.templates[0];
   });
