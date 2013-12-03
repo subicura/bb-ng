1f7bb39f8858b703064b398e0641fc01473abe5a
Kim Daekwon <propellerheaven@gmail.com>
2013-12-04 01:48:59 +0900

    I18n(ko) month names and day names in calendar

--- app/scripts/controllers/app/group/calendar.js
+++ app/scripts/controllers/app/group/calendar.js
@@ -24,6 +24,12 @@ angular.module('bbNgApp')
     /* config object */
     $scope.uiConfig = {
       calendar:{
+        // TODO i18n
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+        dayNamesShort: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+        
         height: 450,
         editable: true,
         header:{
