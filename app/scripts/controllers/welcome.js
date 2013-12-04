37b34080c9df6e882a1be3ff00f8f068d248a8f4
subicura <subicura@subicura.com>
2013-12-05 01:33:46 +0900

    add loading

--- app/scripts/controllers/welcome.js
+++ app/scripts/controllers/welcome.js
@@ -13,28 +13,37 @@ angular.module('bbNgApp')
     $scope.username = 'tester1@bbapi.com';
     $scope.password = '12341234';
 
+    var facebookBtn = Ladda.create( document.querySelector( '#facebook_btn' ) );
+    var emailLoginBtn = Ladda.create( document.querySelector( '#email_login_btn' ) );
+
     $scope.loginWithEmail = function() {
-      LoginService.login(this.username, this.password, function(data, status){
-        $state.go('app.my.timeline');
-        toastr.success('Signed in successfully.')
-      }, function(data, status){      
-        alert(data['message']);
-      });
+      if(!emailLoginBtn.isLoading()) {
+        emailLoginBtn.start();
+        LoginService.login(this.username, this.password, function(data, status){
+          $state.go('app.my.timeline');
+          toastr.success('Signed in successfully.')
+          emailLoginBtn.stop();
+        }, function(data, status){      
+          alert(data['message']);
+          emailLoginBtn.stop();
+        });
+      }
     }
 
     $scope.loginWithFacebook = function() {
-
-      Facebook.getLoginStatus(function(response) {
-        if (response.status == 'connected') {
-          loginWithAuth('facebook', response.authResponse.accessToken);
-        } else {
-          Facebook.login(function(response) {
-            if (response.status == 'connected') {
-              loginWithAuth('facebook', response.authResponse.accessToken);
-            }
-          });
-        }
-      });
+      if(!facebookBtn.isLoading()) {
+        Facebook.getLoginStatus(function(response) {
+          if (response.status == 'connected') {
+            loginWithAuth('facebook', response.authResponse.accessToken);
+          } else {
+            Facebook.login(function(response) {
+              if (response.status == 'connected') {
+                loginWithAuth('facebook', response.authResponse.accessToken);
+              }
+            });
+          }
+        });
+      }
     }
 
     $scope.loginWithTwitter = function() {
@@ -49,11 +58,14 @@ angular.module('bbNgApp')
     // private method 
     
     function loginWithAuth(provider, accessToken){
+      facebookBtn.start();
       LoginService.loginWithAuth(provider, accessToken, function(data, status){
         $state.go('app.my.timeline');
         toastr.success('Signed in successfully.')
+        facebookBtn.stop();
       }, function(data, status){
         alert(data['message']);
+        facebookBtn.stop();
       });
     }    
 
